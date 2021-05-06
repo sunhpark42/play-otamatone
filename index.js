7edea4043c2ee04e04d75da65e8bf588b5aff13d
sunhpark42 <sunhpark42@gmail.com>
2021-05-06 13:58:13 +0900

    feat: 음성 변경 및 버튼, 키보드로 입의 열리고 닫힘을 조절할 수 있게 구현

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-const audioIds = ['first', 'second', 'third', 'fourth'];
+const audioIds = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+  '20',
+  '21',
+  '22',
+  '23',
+  '24',
+];
 
 class Otamatone {
   constructor(root) {
@@ -12,21 +37,43 @@ class Otamatone {
 
   toTemplate() {
     return `
-    <div id="otamatone-chord-slider">
-    ${audioIds
-      .map(
-        (id, index) => `
-          <audio id=${id}>
-          <source src=${['/assets/', index + 1, '.mp3'].join('')} type="audio/mp3">
-          브라우저에서 음성을 재생할 수 없습니다.
-          </audio>
-        `
-      )
-      .join('')}
-      <label id="chord-label">
-        <input type="range" id="chord" name="chord" min="0" max="3" />
-      </label>
-    </div>
+    <div id="otamatone-body">
+      <div id="otamatone-stick">
+        <div id="otamatone-chord-slider">
+        ${audioIds
+          .map(
+            (id) => `
+              <audio id=${'audio' + id}>
+              <source src=${['/assets/', id, '.wav'].join('')} type="audio/wav">
+              브라우저에서 음성을 재생할 수 없습니다.
+              </audio>
+            `
+          )
+          .join('')}
+          <label id="chord-label">
+            <input type="range" id="chord" name="chord" min="0" max='${audioIds.length}' />
+          </label>
+        </div>
+      </div>
+      <div id="otamatone-head">
+        <div id="otamatone-eyes">
+          <div class="otamatone-eye">
+          </div>
+          <div class="otamatone-eye">
+          </div>
+        </div>
+        <div id="otamatone-mouth">
+        </div>
+        <button type="button" id="mouth-button"></button>
+      </div>
+      </div>
+      <div>
+        <ol>
+        <li>회색 부분을 누르면 소리가 나옵니다</li>
+        <li>입 오른쪽 혹은 스페이스키를 누르면 입이 열립니다.</li>
+        (음량조절은 추후에 생길 예정입니다.)
+        </ol>
+      </div>
     `;
   }
 
@@ -39,7 +86,25 @@ class Otamatone {
       const { value: index } = event.target;
       const audioId = audioIds[index];
 
-      document.querySelector(`#${audioId}`).play();
+      document.querySelector(`#${'audio' + audioId}`).play();
+    });
+
+    document.querySelector('#mouth-button').addEventListener('mousedown', () => {
+      document.querySelector('#otamatone-mouth').classList.add('mouth-animate');
+    });
+
+    document.querySelector('#mouth-button').addEventListener('mouseup', () => {
+      document.querySelector('#otamatone-mouth').classList.remove('mouth-animate');
+    });
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key === ' ') {
+        document.querySelector('#otamatone-mouth').classList.add('mouth-animate');
+      }
+    });
+
+    document.addEventListener('keyup', () => {
+      document.querySelector('#otamatone-mouth').classList.remove('mouth-animate');
     });
   }
 }
